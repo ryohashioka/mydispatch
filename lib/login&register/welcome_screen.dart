a2b9d4bc2e0170d8c108646efd6ce344cc4600db
Taku0813 <taku08132001@icloud.com>
2022-10-04 17:29:10 +0900

    画面遷移途中

--- lib/login&register/welcome_screen.dart
+++ lib/login&register/welcome_screen.dart
@@ -7,10 +7,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MainPage(),
-      routes: <String, WidgetBuilder> {
-        '/home': (BuildContext context) => new NewOutsider(),
-        '/subpage': (BuildContext context) => new NewEmployee()
+      title: 'WELLCOME TO MY DISPATCH',
+      theme: ThemeData(
+        primaryColor: Colors.orange,
+      ),
+      routes:  {
+        '/お客様はこちら':(context) => NewOutsider(),
+        '/社員はこちら': (context) => NewEmployee(),
       },
     );
   }
@@ -21,7 +24,7 @@ class MainPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('Navigator'),
+        title: new Text('Select you'),
       ),
       body: new Container(
         padding: new EdgeInsets.all(30.0),
@@ -30,27 +33,6 @@ class MainPage extends StatelessWidget {
             children: <Widget>[
               Text('Main'),
               TextButton(onPressed: () => Navigator.of(context).pushNamed("/subpage"), child: new Text('Subページへ'),)
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-class SubPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Navigator'),
-      ),
-      body: new Container(
-        padding: new EdgeInsets.all(32.0),
-        child: new Center(
-          child: new Column(
-            children: <Widget>[
-              Text('Sub'),
-              TextButton(onPressed: () => Navigator.of(context).pop(), child: new Text('戻る'),)
             ],
           ),
         ),

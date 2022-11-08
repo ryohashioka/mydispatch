00f6de236a1111669b022cd21346ff7a40295e01
Taku0813 <taku08132001@icloud.com>
2022-11-08 16:46:19 +0900

    ログイン画面（あってないかも）

--- lib/login_register/create_outsider.dart
+++ lib/login_register/create_outsider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class NewOutsider extends StatefulWidget {
   @override
@@ -118,10 +119,33 @@ class _NewOutsiderState extends State<NewOutsider> {
               ),
               onChanged: _handleText,
             ),
-            ElevatedButton(onPressed: () => Navigator.of(context).pushNamed('/new_employee'), child: Text('Register')),
+            ElevatedButton(
+                onPressed: () => create(context, 'taku08132001@icloud.com','taku0813'),
+                child: Text('Register')
+            ),
+            // ElevatedButton(onPressed: () => Navigator.of(context).pushNamed('/new_employee'), child: Text('Register')),
           ],
         ),
       ),
     );
+  }
+}
+
+void create(BuildContext context, String email, String password) async {
+  try {
+    final credential = await FirebaseAuth.instance
+        .createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    Navigator.of(context).pop();
+  } on FirebaseAuthException catch (e) {
+    if (e.code == 'weak-password') {
+      print('The password provided is too weak.');
+    } else if (e.code == 'email-already-in-use') {
+      print('The account already exists for that email.');
+    }
+  } catch (e) {
+    print(e);
   }
 }
